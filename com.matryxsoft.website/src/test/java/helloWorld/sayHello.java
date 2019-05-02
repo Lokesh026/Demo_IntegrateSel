900809cea3afe9e31228284b21cbc0b2b39c545e
Lokesh <Lokesh@LAPTOP-KU0RE9GB>
2019-05-02 12:07:18 +0530

    Entire project commit with a Hello world program

---
+++ com.matryxsoft.website/src/test/java/helloWorld/sayHello.java
@@ -0,0 +1,13 @@
+package helloWorld;
+
+import org.testng.annotations.Test;
+
+public class sayHello {
+	
+  @Test
+  public void MatryxsoftWebsite() 
+  {
+	  System.out.println ("Hello Matryxsoft");
+  }
+  
+}
